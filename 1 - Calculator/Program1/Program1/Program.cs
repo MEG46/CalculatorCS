f7bdce71fe6480ad7290b500c21210ac2bd94aa3
Muhammed Esad Goncaloğlu <93635211+MEG46@users.noreply.github.com>
2023-04-11 07:29:51 +0300

    Add files via upload

---
+++ 1 - Calculator/Program1/Program1/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    int num1, num2;
+    char ch;
+    Console.Write("Please enter a number : ");
+    num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    Console.Write("Please enter a number : ");
+    ch = Console.ReadLine()[0];
+    Console.WriteLine();
+    Console.Write("Please enter a number : ");
+    num2 = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (ch == '+')
+    {
+        Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+    }
+    else if (ch == '-')
+    {
+        Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+    }
+    else if (ch == '*' || ch == 'x')
+    {
+        Console.WriteLine(num1 + " x " + num2 + " = " + (num1 * num2));
+    }
+    else if (ch == '/')
+    {
+        if (num2 == 0)
+        {
+            Console.WriteLine("Couldn't Divided By Zero!");
+        }
+        else
+        {
+            Console.WriteLine(num1 + " / " + num2 + " = " + (num1 / num2));
+        }
+    }
+    else if (ch == '%')
+    {
+        if (num2 < 2)
+        {
+            Console.WriteLine("Please enter a bigger number!");
+        }
+        else
+        {
+            Console.WriteLine(num1 + " % " + num2 + " = " + (num1 % num2));
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid process!");
+    }
+}
